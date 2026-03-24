48bb2c1662d889fcea4c048d50040f6abb7e1d0f
HelloWarcraft <wpf@zju.edu.cn>
2026-03-24 13:39:04 +0800

    feat: suit tailwind v4

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {}, // 就改了这一行
-    autoprefixer: {},
-  },
-}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- 1. 引入 Tailwind 插件
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/my-portfolio/' // GitHub 仓库名，前后都要有斜杠
+  plugins: [
+    react(),
+    tailwindcss(), // <-- 2. 启用插件
+  ],
+  base: '/my-portfolio/', // 保持您的 github pages 路径不变
 })
